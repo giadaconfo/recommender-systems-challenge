c1eb999eb4105637ceba01a02011b340f4cfe6e5
Luca Butera <LucaButera@Air-di-Luca.homenet.telecomitalia.it>
2017-10-06 00:08:51 +0200

    Created a more readable dataframe and script to build a correct ICM ICM still not build, estimated required time 20 min of CPU time

--- CBF_try1.py
+++ CBF_try1.py
@@ -1,26 +1,51 @@
 import pandas as pd
 import numpy as np
 import os.path
+from scipy.sparse import coo_matrix
 pd.set_option('display.max_columns',500)
-os.chdir('/Users/LucaButera/git/rschallenge')
+os.chdir('/Users/LucaButera/git/rschallenge') #modify this according to your environment
 
-train = pd.read_csv('Data/train_final.csv','\t')
-tr_info = pd.read_csv('Data/tracks_final.csv','\t')
-pl_info = pd.read_csv('Data/playlists_final.csv','\t')
-tgt_pl = pd.read_csv('Data/target_playlists.csv','\t')
-tgt_tr = pd.read_csv('Data/target_tracks.csv','\t')
+#Creates the ICM
+def createICM(tracks):
+    #auxiliary vars
+    tg = ['tag1','tag2','tag3','tag4','tag5']
+    sel = ['artist_id', 'album'] + tg
 
-def splitnflat(v):
-    res = []
-    for x in np.nditer(v, flags=['refs_ok']):
-        new = [int(j[0]) for j in [x.item(0)[1:-1].split(',')] if j[0] != 'None' and j[0] != '']
-        res = res + new
-    return np.unique(np.array(res))
+    items = np.unique(tracks['track_id'].values)
+    artists = np.unique(tracks['artist_id'].values)
+    albums = np.unique(tracks['album'].values)
+    tags = np.unique(tracks[tg].values)
 
-items = tr_info['track_id'].values
-albums = splitnflat(tr_info['album'].values)
-tags = splitnflat(tr_info['tags'].values)
-attr = np.append(albums, tags)
+    #Structure to index the sparse ICM matrix using ids
+    ICM_items = pd.Series(range(items.size), index=items)
+    ICM_artists = pd.Series(range(artists.size), index=artists)
+    ICM_albums = pd.Series(range(artists.size, artists.size + albums.size), index=albums)
+    ICM_tags = pd.Series(range(artists.size + albums.size, artists.size + albums.size + tags.size), index=tags)
 
-ICM = pd.DataFrame(index=attr, columns=items)
-print(ICM.shape)
+    #Creating ICM
+    rows = []
+    columns = []
+    for i,r in tr_info.set_index('track_id').iterrows():
+        columns += [ICM_items[i]]*r[sel].count()
+        if  r['artist_id'] != 'NaN': rows += [ICM_artists[r['artist_id']]]
+        if r['album'] != 'NaN': rows += [ICM_albums[r['album']]]
+        if r['tag1'] != 'NaN': rows += [ICM_tags[r['tag1']]] + [ICM_tags[r['tag2']]] + [ICM_tags[r['tag3']]] + [ICM_tags[r['tag4']]] + [ICM_tags[r['tag5']]]
+
+    data = [1]*len(rows)
+
+    ICM = coo_matrix(data,(rows,columns))
+
+    return ICM, ICM_items, ICM_artists, ICM_albums, ICM_tags
+
+#Importing the data
+tr_info = pd.read_csv('Data/fixed_tracks_final.csv')
+
+#Creating ICM plus auxiliary structures
+ICM, ICM_items, ICM_artists, ICM_albums, ICM_tags = createICM(tr_info)
+
+#Saving everything
+save_npz('BuiltStructures/ICM/ICM.npz',ICM)
+ICM_items.to_csv('BuiltStructures/ICM/ICM_items.csv')
+ICM_artists.to_csv('BuiltStructures/ICM/ICM_artists.csv')
+ICM_albums.to_csv('BuiltStructures/ICM/ICM_albums.csv')
+ICM_tags.to_csv('BuiltStructures/ICM/ICM_tags.csv')
